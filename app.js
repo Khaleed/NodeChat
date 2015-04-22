7a6202637f017f59d82e47f6ef1650414aa22e5c
Khalid <Khalid.Ali@consultant.com>
2015-04-22 14:40:26 -0400

    create client.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 var publicDir = "./public/";
 // define route
-app.get("/", function(request, response) {
-	response.sendFile(publicDir + "index.html");
+app.get("/", function (request, response) {
+    response.sendFile(publicDir + "index.html");
 });
 // listen to port or whatever is in process.env.port
 var port = process.env.port || 3000;
@@ -20,21 +20,21 @@ var io = require("socket.io")(server);
 var mongoose = require("mongoose");
 // open a connection to the node_chat db on locally running instance
 // of MongoDB 
-mongoose.connect("mongodb://localhost/node_chat", function(err) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log("mongdodb connection created");
-	}
+mongoose.connect("mongodb://localhost/node_chat", function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("mongdodb connection created");
+    }
 });
 // map Schema to MongoDB collection
 var chatSchema = mongoose.Schema({
-	user: String,
-	msg: String,
-	updated: {
-		type: Date,
-		default: Date.now
-	}
+    user: String,
+    msg: String,
+    updated: {
+        type: Date,
+        default: Date.now
+    }
 });
 // compile Schema into a model
 // instances of model represent documents
@@ -42,71 +42,71 @@ var chatSchema = mongoose.Schema({
 var NodeChat = mongoose.model("Message", chatSchema);
 var usernames = [];
 // client connects to socket.io
-io.on("connection", function(client) {
-	console.log("Socket.io connection established");
-	client.on("join", function(nick, callback) {
-		// retreive old messages
-		// return an instance of Query
-		// and use QueryBuilder interface
-		var query = NodeChat.find({}).sort("-updated").limit(10).exec(function(err, results) {
-			if (err) {
-				console.error(err);
-			} else {
-				client.emit("old msg", results);
-			}
-		});
-		//check if usernames is not in array
-		if (usernames.indexOf(nick) === -1) {
-			callback(true);
-			// make nick as property of client's socket
-			client.nickname = nick;
-			// push nick associate with each socket into usernames array
-			usernames.push(client.nickname);
-			// emit all nicknames to users
-			io.emit("nicks", usernames);
-		} else {
-			callback(false);
-		}
-	});
-	// listen for messages from client
-	client.on("messages", function(data) {
-		// create a new instance of NodeChat model
-		var chatMsg = new NodeChat({
-			user: client.nickname,
-			msg: data
-		});
-		// save messages on MongoDB
-		chatMsg.save(function(err) {
-			if (err) {
-				console.error(err);
-			} else {
-				// emit new messages to all clients
-				io.emit("new messages", {
-					user: client.nickname,
-					msg: data
-				});
-			}
-		});
-	});
-	client.on("disconnect", function() {
-		console.log(client.nickname + " logged off");
-		var start = usernames.indexOf(client.nickname);
-		// check if client has the property nickname
-		if (client.nickname) {
-			// remove nick from usernames array
-			usernames.splice(start, 1);
-			// emit updated list of nicks
-			io.emit("nicks", usernames);
-			// if client doesn't have nickname property, do nothing
-		} else {
-			return;
-		}
-	});
+io.on("connection", function (client) {
+    console.log("Socket.io connection established");
+    client.on("join", function (nick, callback) {
+        // retreive old messages
+        // return an instance of Query
+        // and use QueryBuilder interface
+        var query = NodeChat.find({}).sort("-updated").limit(10).exec(function (err, results) {
+            if (err) {
+                console.error(err);
+            } else {
+                client.emit("old msg", results);
+            }
+        });
+        //check if usernames is not in array
+        if (usernames.indexOf(nick) === -1) {
+            callback(true);
+            // make nick as property of client's socket
+            client.nickname = nick;
+            // push nick associate with each socket into usernames array
+            usernames.push(client.nickname);
+            // emit all nicknames to users
+            io.emit("nicks", usernames);
+        } else {
+            callback(false);
+        }
+    });
+    // listen for messages from client
+    client.on("messages", function (data) {
+        // create a new instance of NodeChat model
+        var chatMsg = new NodeChat({
+            user: client.nickname,
+            msg: data
+        });
+        // save messages on MongoDB
+        chatMsg.save(function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                // emit new messages to all clients
+                io.emit("new messages", {
+                    user: client.nickname,
+                    msg: data
+                });
+            }
+        });
+    });
+    client.on("disconnect", function() {
+        console.log(client.nickname + " logged off");
+        var start = usernames.indexOf(client.nickname);
+        // check if client has the property nickname
+        if (client.nickname) {
+            // remove nick from usernames array
+            usernames.splice(start, 1);
+            // emit updated list of nicks
+            io.emit("nicks", usernames);
+            // if client doesn't have nickname property, do nothing
+        } else {
+            return;
+        }
+    });
 });
 server.on("listening", function() {
-	console.log("OK, the server is listening ");
+    console.log("OK, the server is listening ");
 });
 // listen to port or whatever is in process.env.port
 server.listen(port, function() {
-	console.log("listening on port " + port);
+    console.log("listening on port " + port);
 });
---
+++ public/client.js
@@ -0,0 +1,72 @@
+// invoke io 
+var socket = io();
+// connect io to localhost:3000
+socket.connect("http://localhost:3000");
+// get elements
+var mainHeader = document.getElementById("main-header"),
+    userForm = document.getElementById("user-form"),
+    // elem to print errors in entering nicknames
+    nickErr = document.getElementById("user-err"),
+    // elem to hold all nicks
+    nickElem = document.getElementById("nicks"),
+    userInput = document.getElementById("user-input"),
+    // elem to show and hide nick form
+    userWrapper = document.getElementById("user-wrapper"),
+    // chat elems
+    chatForm = document.getElementById("chat-form"),
+    chatInput = document.getElementById("chat-input"),
+    chatBox = document.getElementById("chat-box"),
+    chatMsg = document.getElementById("chat-msg"),
+    chatContent = document.getElementById("chat-container"),
+    chatWrapper = document.getElementById("chat-wrapper");
+// bind nicknames event handler
+userForm.addEventListener("submit", function(e) {
+    e = e || event; // work-around for IE
+    e.preventDefault();
+    // emit join event  to server with user input and callback 
+    socket.emit("join", userInput.value, function(isNickAvailable) {
+        if (isNickAvailable) {
+            // hide username input area
+            userWrapper.style.display = "none";
+            // clear welcome message
+            mainHeader.innerHTML = "";
+            // start the chat page
+            chatWrapper.style.display = "block";
+        } else {
+            nickErr.innerHTML = "Nick already taken, please select another name";
+        }
+    });
+    userInput.value = "";
+});
+// listen for nicks event and display nicknames
+socket.on("nicks", function(nicks) {
+    var i, len = nicks.length,
+        str = "";
+    for (i = 0; i < len; i += 1) {
+        // delimit strings into multiple lines
+        str += nicks[i] + "</br>";
+    }
+    nickElem.innerHTML = str;
+});
+// bind chat messages event handler
+chatForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    // emit messages event to server
+    socket.emit("messages", chatInput.value);
+    chatInput.value = "";
+});
+// helper function to display messages
+function addMsg(data) {
+        chatMsg.innerHTML += ("<i>" + data.user + ": </i>" + data.msg + "</br>");
+    }
+    // listen for new messages event from server
+socket.on("new messages", function(data) {
+    addMsg(data);
+});
+// listen for old messages event from server
+socket.on("old msg", function(data) {
+    var i, len = data.length;
+    for (i = 0; i < len; i += 1) {
+        addMsg(data[i]);
+    }
+});
