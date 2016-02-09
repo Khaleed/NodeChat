fafa69619bd6a41e3efdf155b31b1993bb35035a
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-09 03:11:20 +0000

    cleanup

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 var express = require("express");
-// declare a new instance of express
 var app = express();
-// app is initialised as a function handler and supplied as
-// arg to the HTTP server
 var server = require("http").Server(app);
-// require path to handle and transform file paths
 var path = require("path");
 // serve static files via express
 app.use(express.static(path.join(__dirname, "public")));
 var publicDir = "./public/";
 // define route
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
     response.sendFile(publicDir + "index.html");
 });
-// listen to port or whatever is in process.env.port
 var port = process.env.port || 3000;
 // require socket.io and pass server
 var io = require("socket.io")(server);
+// interface to talk to mongodb
 var mongoose = require("mongoose");
-// open a connection to the node_chat db on locally running instance
-// of MongoDB 
-mongoose.connect("mongodb://localhost/node_chat", function (err) {
+// create node_chat db
+mongoose.connect("mongodb://localhost/node_chat", function(err) {
     if (err) {
         console.error(err);
     } else {
@@ -39,16 +34,12 @@ var chatSchema = mongoose.Schema({
 // compile Schema into a model
 // instances of model represent documents
 // which can be saved or retrieved from db
-var NodeChat = mongoose.model("Message", chatSchema);
+var Messages = mongoose.model("Message", chatSchema);
 var usernames = [];
-// client connects to socket.io
-io.on("connection", function (client) {
+io.on("connection", function(client) {
     console.log("Socket.io connection established");
-    client.on("join", function (nick, callback) {
-        // retreive old messages
-        // return an instance of Query
-        // and use QueryBuilder interface
-        var query = NodeChat.find({}).sort("-updated").limit(10).exec(function (err, results) {
+    client.on("join", function(nick, callback) {
+        var query = Messages.find({}).sort("-updated").limit(10).exec(function(err, results) {
             if (err) {
                 console.error(err);
             } else {
@@ -60,7 +51,7 @@ io.on("connection", function (client) {
             callback(true);
             // make nick as property of client's socket
             client.nickname = nick;
-            // push nick associate with each socket into usernames array
+            // push nick associated with each socket into usernames array
             usernames.push(client.nickname);
             // emit all nicknames to users
             io.emit("nicks", usernames);
@@ -69,14 +60,14 @@ io.on("connection", function (client) {
         }
     });
     // listen for messages from client
-    client.on("messages", function (data) {
-        // create a new instance of NodeChat model
-        var chatMsg = new NodeChat({
+    client.on("messages", function(data) {
+        // create a new instance of Messages model
+        var chatMsg = new Messages({
             user: client.nickname,
             msg: data
         });
         // save messages on MongoDB
-        chatMsg.save(function (err) {
+        chatMsg.save(function(err) {
             if (err) {
                 console.error(err);
             } else {
